70ea7e4daea0c0dbccc6d06f809a04a235f47702
雅楠苏 <riven_rabbit@163.com>
2020-09-22 14:55:30 +0800

    登录

--- app/src/main/java/com/renard/app/MainActivity.java
+++ app/src/main/java/com/renard/app/MainActivity.java
@@ -1,31 +1,35 @@
 package com.renard.app;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.renard.rhsdk.RHSDK;
+import com.renard.rhsdk.log.Log;
 import com.renard.rhsdk.sdk.RHSDKCode;
 import com.renard.rhsdk.sdk.RHSDKInitListener;
 
-public class MainActivity extends Activity {
+import static com.renard.rhsdk.RHSDK.*;
+
+public class MainActivity extends AppCompatActivity {
     private Activity context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        context=this;
         init();
     }
 
     private void init() {
-        RHSDK.getInstance().init(this, new RHSDKInitListener() {
+        getInstance().init(this, new RHSDKInitListener() {
             @Override
             public void onInitResult(int code, String msg) {
                 if (code== RHSDKCode.CODE_INIT_SUCCESS){
                     Log.d("初始化成功","+++++++++++++++++++++");
-                    RHSDK.getInstance().login(context);
+                    getInstance().login(context);
                 }
             }
 
@@ -50,4 +54,5 @@ public class MainActivity extends Activity {
             }
         });
     }
+
 }
---
+++ rhsdk/src/main/java/com/renard/rhsdk/SSYUser.java
@@ -0,0 +1,47 @@
+package com.renard.rhsdk;
+
+import android.app.Activity;
+
+import com.renard.rhsdk.log.Log;
+import com.renard.rhsdk.plugin.UserPlugin;
+import com.renard.rhsdk.sdk.RHSDKCode;
+import com.renard.rhsdk.sdk.RHSDKManager;
+import com.renard.rhsdk.user.UserAdapter;
+import com.renard.rhsdk.user.UserExtraData;
+import com.renard.rhsdk.util.Arrays;
+
+/**
+ * Created by Riven_rabbit on 2020/9/18
+ *
+ * @author suyanan
+ */
+public class SSYUser extends UserAdapter implements UserPlugin {
+
+    public String[] supportedMethods = {"login","logout","submitExtraData"};
+
+    public SSYUser(Activity activity) {
+        Log.e("111","111");
+        RHSDKManager.getInstance().onResult(
+                RHSDKCode.CODE_INIT_SUCCESS, "init success");
+    }
+
+    @Override
+    public boolean isSupportMethod(String methodName) {
+        return Arrays.contain(supportedMethods, methodName);
+    }
+
+    @Override
+    public void login() {
+        Log.e("222","222");
+    }
+
+    @Override
+    public void logout() {
+
+    }
+
+    @Override
+    public void submitExtraData(UserExtraData extraData) {
+
+    }
+}
